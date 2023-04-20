10b60a2d93f2cf0b137b9d31ba0269ea0370d405
CarlosVasquezCV <carlos.vasquez180@misena.edu.co>
2023-04-20 09:16:09 -0500

    parte 2 ejercicio practico

--- js/script.js
+++ js/script.js
@@ -5,5 +5,10 @@ let numberTwo;
 let name="Michel";
 let age=17;
 let state;
+let more;
 const iva=19;//constante
 console.log (`mi nombre es ${name} y tengo ${age} años, el iva de mi edad es ${age*(iva/100)}`);//usp de plantillas literales
+numberOne=parseInt(prompt("Ingrese el primer número"));
+numberTwo=parseInt(prompt("Ingrese el segundo número"));
+more=numberOne+numberTwo;
+alert("la suma es "+more);
