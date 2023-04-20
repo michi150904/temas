e1001c977edfa3cc58b7c5064f8eaf0bb175a066
CarlosVasquezCV <carlos.vasquez180@misena.edu.co>
2023-04-20 08:57:28 -0500

    ejercicio practico java script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("hola desde afuera");//archivo que se usa para el metodo externo
+//declaración de variables
+let numberOne;
+let numberTwo;
+let name="Michel";
+let age=17;
+let state;
+const iva=19;//constante
+console.log (`mi nombre es ${name} y tengo ${age} años, el iva de mi edad es ${age*(iva/100)}`);//usp de plantillas literales
